b491592ad4f33f5244031d517131b955b72861ef
Paan <paan.kaur@gmail.com>
2025-09-10 10:45:38 +0300

    Week 1: ex9 and ex14

---
+++ week1/Positive.java
@@ -0,0 +1,16 @@
+package week1;
+
+import java.util.Scanner;
+
+public class Positive {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please test a number: ");
+        int result = Integer.parseInt(input.nextLine());
+        if (result < 0) {
+            System.out.println("Your number is negative");
+        } else  {
+            System.out.println("Your number is positive");
+        }
+    }
+}
--- week1/Spruce.java
+++ week1/Spruce.java
@@ -1,7 +1,15 @@
 package week1;
 
+import java.util.Scanner;
+
 public class Spruce {
     public static void main(String[] args) {
-        System.out.println("ohy");
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type a number: ");
+        int number1 = Integer.parseInt(reader.nextLine());
+        System.out.print("..and a second number: ");
+        int number2 = Integer.parseInt(reader.nextLine());
+        double answer = (double) number1 / number2;
+        System.out.println(number1 + " divided by "  + number2 + " is: " + answer) ;
     }
 }
